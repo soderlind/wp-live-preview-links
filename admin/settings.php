9fede3e6590751f5a1433d3ebd76dff668df8264
soderlind <per@soderlind.no>
2013-12-25 00:28:21 +0100

    upgraded admin-page-framework enqueued  javascript and css created javascript test file

--- admin/settings.php
+++ admin/settings.php
@@ -4,38 +4,36 @@
 if ( ! class_exists( 'AdminPageFramework' ) )
 	include_once dirname( __FILE__ ) . '/admin-page-framework.php';
 
-class WP_Live_Preview_Post_Settings extends AdminPageFramework {
+class WP_Live_Preview_Links_Settings extends AdminPageFramework {
 
 	public function setUp() {
 		$this->setRootMenuPage(
-			__('Live Preview Post','wp-live-preview-post' ),
+			__('Live Preview Links','wp-live-preview-links' ),
 			//plugins_url( 'img/keyhole.png', __FILE__ )
 			"media"
 		);
 
-
-
 		$this->showPageHeadingTabs( false );
 
 		$this->addSubMenuPage(
-			__('Live Preview Post','wp-live-preview-post' ),
-			'wp_live_preview_post_options',
+			__('Live Preview Links','wp-live-preview-links' ),
+			'wp_live_preview_links_options',
 			'options-general'
 		);
 
 		$this->addHelpTab(
 			array(
-				'strPageSlug'         => 'wp_live_preview_post_options',
-				'strHelpTabTitle'     => __('Live Preview Post','wp-live-preview-post' ),
+				'strPageSlug'         => 'wp_live_preview_links_options',
+				'strHelpTabTitle'     => __('Live Preview Post','wp-live-preview-links' ),
 				'strHelpTabID'        => 'general_options_help_introduction',
-				'strHelpTabContent'   => __( 'This contextual help text can be set with the addHelpTab() method.', 'wp-live-preview-post' ),
+				'strHelpTabContent'   => __( 'This contextual help text can be set with the addHelpTab() method.', 'wp-live-preview-links' ),
 			)
 		);
 
 		$this->addSettingSections(
 			array(
 				'strSectionID'       => 'options',
-				'strPageSlug'        => 'wp_live_preview_post_options'
+				'strPageSlug'        => 'wp_live_preview_links_options'
 			)
 		);
 
@@ -43,27 +41,27 @@ class WP_Live_Preview_Post_Settings extends AdminPageFramework {
 			array(
 				'strFieldID'		=> 'link',
 				'strSectionID'       => 'options',
-				'strTitle'			=> __( 'Live Preview Links','wp-live-preview-post' ),
-				'strDescription'	=> __( "Which links will trigger the live preview. Certain external sites may have set their X-FRAME-OPTIONS header policy to SAMEORGIN or DENY. This is specifically to prevent other sites from iframing their site for obvious reasons. If that is the case, this plugin will not work, and it's best to respect the site owner's wishes.", 'wp-live-preview-post' ),
+				'strTitle'			=> __( 'Live Preview Links','wp-live-preview-links' ),
+				'strDescription'	=> __( "Which links will trigger the live preview. Certain external sites may have set their X-FRAME-OPTIONS header policy to SAMEORGIN or DENY. This is specifically to prevent other sites from iframing their site for obvious reasons. If that is the case, this plugin will not work, and it's best to respect the site owner's wishes.", 'wp-live-preview-links' ),
 				'strType'			=> 'select',
 				'vLabel'            => array( 
-					'site'        => __( 'Site internal links only', 'wp-live-preview-post' ),
-					'external'    => __( 'External links only', 'wp-live-preview-post' ),
-					'all'         => __( 'All links', 'wp-live-preview-post' ),
-					'class'       => __( 'class="livepreview"', 'wp-live-preview-post' ),
-					'shortcode'   => __( '[livepreview] shortcode', 'wp-live-preview-post' )
+					'site'        => __( 'Site internal links only', 'wp-live-preview-links' ),
+					'external'    => __( 'External links only', 'wp-live-preview-links' ),
+					'all'         => __( 'All links', 'wp-live-preview-links' ),
+					'class'       => __( 'class="livepreview"', 'wp-live-preview-links' ),
+					'shortcode'   => __( '[livepreview] shortcode', 'wp-live-preview-links' )
 				),
 				'vDefault' 			=> 'site'
 			),
 			array(
 				'strFieldID'         => 'dialog',
 				'strSectionID'       => 'options',
-				'strTitle'           => __( 'Live Preview Dialog', 'wp-live-preview-post' ),
-				'strDescription'     => __( 'The preview dialog size.', 'wp-live-preview-post' ),
+				'strTitle'           => __( 'Live Preview Dialog', 'wp-live-preview-links' ),
+				'strDescription'     => __( 'The preview dialog size.', 'wp-live-preview-links' ),
 				'strType'            => 'size',
 				'vLabel'             => array(
-					'width'    => __( 'Width', 'wp-live-preview-post' ),
-					'height'   => __( 'Height', 'wp-live-preview-post' ),
+					'width'    => __( 'Width', 'wp-live-preview-links' ),
+					'height'   => __( 'Height', 'wp-live-preview-links' ),
 				),
 				'vSizeUnits'         => array(
 					'width'    => array( 'px' => 'px', '%' => '%', 'em' => 'em' ),
@@ -73,17 +71,21 @@ class WP_Live_Preview_Post_Settings extends AdminPageFramework {
 					'width'    => array( 'size' => 300, 'unit' => 'px' ),
 					'height'   => array( 'size' => 200, 'unit' => 'px' ),
 				),
+				'vMin'               => array(
+					'width'    => 0,
+					'height'   => 0
+				),
 				'vDelimiter'       => '<br />',
 			),
 			array(
 				'strFieldID'         => 'target',
 				'strSectionID'       => 'options',
-				'strTitle'           => __( 'Site Viewport', 'wp-live-preview-post' ),
-				'strDescription'     => __( 'The viewport size of the site you are previewing', 'wp-live-preview-post' ),
+				'strTitle'           => __( 'Site Viewport', 'wp-live-preview-links' ),
+				'strDescription'     => __( 'The viewport size of the site you are previewing', 'wp-live-preview-links' ),
 				'strType'            => 'size',
 				'vLabel'             => array(
-					'width'  => __( 'Width', 'wp-live-preview-post' ),
-					'height' => __( 'Height', 'wp-live-preview-post' ),
+					'width'  => __( 'Width', 'wp-live-preview-links' ),
+					'height' => __( 'Height', 'wp-live-preview-links' ),
 				),
 				'vSizeUnits'         => array(
 					'width'  => array( 'px' => 'px', '%' => '%', 'em' => 'em' ),
@@ -93,25 +95,29 @@ class WP_Live_Preview_Post_Settings extends AdminPageFramework {
 					'width'  => array( 'size' => 1000, 'unit' => 'px' ),
 					'height' => array( 'size' => 800, 'unit' => 'px' ),
 				),
+				'vMin'               => array(
+					'width'  => 0,
+					'height' => 0
+				),
 				'vDelimiter'         => '<br />',
 			),
 			array(
 				'strFieldID'         => 'scale',
 				'strSectionID'       => 'options',
-				'strTitle'           => __( 'Scale', 'wp-live-preview-post' ),
+				'strTitle'           => __( 'Scale', 'wp-live-preview-links' ),
 				'strType'            => 'number',
 				'vMax'               => '1.0',
 				'vMin'               => '0.0',
 				'vStep'              => '0.1',
-				'strDescription'     => __( 'The scaling of the viewport size of the site you are previewing (this is the CSS transform scale property), default = calculated automatically. Notes: If no scaling is specified (0), then the scaling is automatically calculated to provide the best fit to the preview dialog window size.', 'wp-live-preview-post' ),
+				'strDescription'     => __( 'The scaling of the viewport size of the site you are previewing (this is the CSS transform scale property), default = calculated automatically. Notes: If no scaling is specified (0), then the scaling is automatically calculated to provide the best fit to the preview dialog window size.', 'wp-live-preview-links' ),
 				'vDefault'           =>  0,
 				//'vSize'              => 40
 			),
 			array(
 				'strFieldID'         => 'offset',
 				'strSectionID'       => 'options',
-				'strTitle'           => __( 'Offset', 'wp-live-preview-post' ),
-				'strDescription'     => __( 'The offset from the target in pixels', 'wp-live-preview-post' ),
+				'strTitle'           => __( 'Offset', 'wp-live-preview-links' ),
+				'strDescription'     => __( 'The offset from the target in pixels', 'wp-live-preview-links' ),
 				'strType'            => 'size',
 				'vSizeUnits'         => array( 'px' => 'px', '%' => '%', 'em' => 'em' ),
 				'vMax'               => '500',
@@ -123,8 +129,8 @@ class WP_Live_Preview_Post_Settings extends AdminPageFramework {
 			array(
 				'strFieldID'         => 'postition',
 				'strSectionID'       => 'options',
-				'strTitle'           => __( 'Postition', 'wp-live-preview-post' ),
-				'strDescription'     => __( 'Side to which the preview will open', 'wp-live-preview-post' ),
+				'strTitle'           => __( 'Postition', 'wp-live-preview-links' ),
+				'strDescription'     => __( 'Side to which the preview will open', 'wp-live-preview-links' ),
 				'strType'            => 'radio',
 				'vLabel'             => array( 'left' => 'Left', 'right' => 'Right'  ),
 				'vDefault'           => 'right'
@@ -133,10 +139,49 @@ class WP_Live_Preview_Post_Settings extends AdminPageFramework {
 
 	}
 
-	public function do_WP_Live_Preview_Post_Settings() {
+	public function do_WP_Live_Preview_Links_Settings() {
         submit_button();
-        //update_option( 'WP_Live_Preview_Post_Settings', '' );
-        echo $this->oDebug->getArray( get_option( 'WP_Live_Preview_Post_Settings' ) );       
+        //update_option( 'WP_Live_Preview_Links_Settings', '' );
+        echo $this->oDebug->getArray( get_option( 'WP_Live_Preview_Links_Settings' ) );       
     }
+
+
+    public function validation_WP_Live_Preview_Links_Settings( $arrNewInput, $arrOldOptions ) {	// validation_ + {extended class name} + _ + {field ID}
+		
+		// Set a flag.
+		$fVerified = true;
+		
+		// We store values that have an error in an array and pass it to the setFieldErrors() method.
+		// It internally stores the error array in a temporary area of the database called transient.
+		// The used name of the transient is a md5 hash of 'instantiated class name' + '_' + 'page slug'. 
+		// The library class will search for this transient when it renders the form fields 
+		// and if it is found, it will display the error message set in the field array. 
+		$arrErrors = array();
+
+
+
+
+		// Check if the submitted value meets your criteria. As an example, here a numeric value is expected.
+		if ( ! is_numeric( $arrNewInput['wp_live_preview_links_options']['options']['dialog']['width']['size'] ) ) {
+			
+			// Start with the section key in $arrErrors, not the key of page slug.
+			$arrErrors['options']['dialog'] = 'The value must be numeric: ' . $arrNewInput['wp_live_preview_links_options']['options']['dialog']['width']['size'];	
+			$fVerified = false;
+			
+		}
+		
+		// An invalid value is found.
+		if ( ! $fVerified ) {
+		
+			// Set the error array for the input fields.
+			$this->setFieldErrors( $arrErrors );		
+			$this->setSettingNotice( 'There was an error in your input.' );
+			return $arrOldOptions;
+			
+		}
+				
+		return $arrNewInput;		
+		
+	}
 
 }
--- wp-live-preview-links.php
+++ wp-live-preview-links.php
@@ -1,6 +1,6 @@
 <?php
 /* 
-    Plugin Name: WP Live Preview Post
+    Plugin Name: WP Live Preview Links
     Plugin URI: http://soderlind.no/
     Description: lorem ipsum dolores est
     Author: Per Soderlind
@@ -17,5 +17,25 @@ if ( is_admin() ) {
 function wp_live_preview_links_init() {
 	load_plugin_textdomain( 'wp-live-preview-links', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );; 
 }
+
+
+function wp_live_preview_links_styles()
+{
+	// Register the style like this for a plugin:
+	wp_register_style( 'live-preview', plugins_url( '/lib/css/livepreview-demo.css', __FILE__ ), array(), '1.0', 'all' );
+	wp_enqueue_style( 'live-preview' );
+}
+add_action( 'wp_enqueue_scripts', 'wp_live_preview_links_styles' );
+
+function wp_live_preview_links_scripts()
+{
+	wp_enqueue_script( 'jquery-live-preview', plugins_url( '/lib/js/jquery-live-preview.js', __FILE__ ),array( 'jquery' ),'1.1' );
+	wp_enqueue_script( 'wp-live-preview-links-js', plugins_url( '/lib/js/jquery-wp-live-preview-links.js', __FILE__ ),array( 'jquery','jquery-live-preview' ),'1.1' );	
+	//wp_enqueue_script( 'jquery-live-preview' );
+	//wp_enqueue_script( 'wp-live-preview-links-js' );	
+
+}
+add_action( 'wp_enqueue_scripts', 'wp_live_preview_links_scripts', 5 );
+
 
 
